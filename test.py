5bef691df675a150525dd6608b5fce67f7db3327
Gaétan Dégot-Silvestre <gaetan.degot@gmail.com>
2026-09-01 22:36:48 +0200

    advancement

--- test.py
+++ test.py
@@ -5,3 +5,4 @@ import dotenv
 dotenv.load_dotenv()
 
 print("CLEARSY_API_KEY:", dotenv.get_key(dotenv.find_dotenv(), "CLEARSY_API_KEY"))
+print("CLEARSY_API_KEY:", dotenv.get_key(dotenv.find_dotenv(), "CLEARSY_API_KEY"))
